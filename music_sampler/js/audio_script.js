a33884a05aac9530b26a04504107a8f74cf58adb
vrmcdonald <43391969+vrmcdonald@users.noreply.github.com>
2018-11-17 00:03:32 -0500

    Add files via upload

---
+++ music_sampler/js/audio_script.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+
+    $('#hm').on('click', () => {
+       $('#song_1').slideToggle();
+    });
+
+    $('#d').on('click', () => {
+       $('#song_2').slideToggle();
+    });
+
+    $('#ed').on('click', () => {
+       $('#song_3').slideToggle();
+    });
+
+    $('#sa').on('click', () => {
+       $('#song_4').slideToggle();
+    });
+});
